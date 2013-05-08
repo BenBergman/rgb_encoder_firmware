6a808c5a1947b7d7d6c5e475952a2705853b9552
Benjamin Bergman <ben@benbergman.ca>
2013-05-08 08:50:42 -0500

    Factor out LED number to bit conversion

--- src/LedDriver.c
+++ src/LedDriver.c
@@ -8,18 +8,21 @@ void LedDriver_Create(uint8_t *leds)
 	*leds = 0;
 }
 
+static uint8_t convertLedNumberToBit(int ledNumber)
+{
+	switch (ledNumber) {
+		case 1:
+			return 0x01;
+		case 2:
+			return 0x20;
+		default:
+			return 0;
+	}
+}
+
 void LedDriver_TurnOn(int led)
 {
-	switch (led) {
-		case 1:
-			*ledsAddress |= 0x01;
-			break;
-		case 2:
-			*ledsAddress |= 0x20;
-			break;
-		default:
-			break;
-	}
+	*ledsAddress |= convertLedNumberToBit(led);
 }
 
 void LedDriver_TurnOff(int led)
