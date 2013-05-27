7a48647593fc3400a9479a31d2067e0722c5f473
Benjamin Bergman <ben@benbergman.ca>
2013-05-27 08:17:11 -0500

    White space fixes

--- main.c
+++ main.c
@@ -5,8 +5,8 @@
 #define pin6mask  (0x01 << 6)
 
 int main(void) {
-    /* Hold the watchdog timer so it doesn't reset our chip */
-    WDTCTL = WDTPW + WDTHOLD;
+	/* Hold the watchdog timer so it doesn't reset our chip */
+	WDTCTL = WDTPW + WDTHOLD;
 	_BIS_SR(OSCOFF);                          // XTAL not used
 	if (CALBC1_16MHZ ==0xFF || CALDCO_16MHZ == 0xFF)
 	{
@@ -18,14 +18,14 @@ int main(void) {
 	BCSCTL1 = CALBC1_16MHZ;  // Set DCO to 16MHz
 	DCOCTL = CALDCO_16MHZ;
 
-    P1DIR = 0x03;
+	P1DIR = 0x03;
 	P2DIR &= ~(0b11000000);
 	P2REN |= 0b11000000;
 
 	RgbLedDriver_Create();
 
-    /* infinite loop */
-    for( ; ; ) {
+	/* infinite loop */
+	for( ; ; ) {
 		RgbLedDriver_TurnOn(1 , 5, 1, 1);
 		RgbLedDriver_TurnOn(2 , 1, 5, 1);
 		RgbLedDriver_TurnOn(3 , 1, 1, 5);
@@ -42,5 +42,5 @@ int main(void) {
 		RgbLedDriver_TurnOn(14, 1, 5, 1);
 		RgbLedDriver_TurnOn(15, 1, 1, 5);
 		RgbLedDriver_TurnOn(16, 5, 5, 5);
-    }
+	}
 }
