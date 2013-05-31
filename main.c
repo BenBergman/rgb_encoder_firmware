c5acd87a110170ee1c933082732f18ba9c939c9b
Benjamin Bergman <ben@benbergman.ca>
2013-05-31 14:20:01 -0500

    Refactor main

--- main.c
+++ main.c
@@ -3,12 +3,22 @@
 #include "RgbLedDriver.h"
 #include "RotaryEncoder.h"
 
-#define pin6mask  (0x01 << 6)
+void initializeEncoderRegisters(void)
+{
+	_BIS_SR(OSCOFF);          // XTAL not used
+	P2DIR &= ~(BIT7|BIT6);
+	P2SEL &= ~(BIT7|BIT6);
+	P2SEL2 &= ~(BIT7|BIT6);
+	P2REN |= BIT7|BIT6;
+}
 
-int main(void) {
-	/* Hold the watchdog timer so it doesn't reset our chip */
-	WDTCTL = WDTPW + WDTHOLD;
-	_BIS_SR(OSCOFF);                          // XTAL not used
+void initializeLedRegisters(void)
+{
+	P1DIR = BIT0;
+}
+
+void initializeClock(void)
+{
 	if (CALBC1_16MHZ ==0xFF || CALDCO_16MHZ == 0xFF)
 	{
 		/* If calibration constants erased
@@ -18,36 +28,45 @@ int main(void) {
 	}
 	BCSCTL1 = CALBC1_16MHZ;  // Set DCO to 16MHz
 	DCOCTL = CALDCO_16MHZ;
+}
 
-	P1DIR = 0x01;
-	P2DIR &= ~(0b11000000);
-	P2SEL &= ~(0b11000000);
-	P2SEL2 &= ~(BIT7|BIT6);
-	P2REN |= 0b11000000;
+static int i = 0;
+
+void loop(void)
+{
+	RgbLedDriver_TurnOn((1  - i + 16) % 16 + 1, 5, 1, 1);
+	RgbLedDriver_TurnOn((2  - i + 16) % 16 + 1, 1, 5, 1);
+	RgbLedDriver_TurnOn((3  - i + 16) % 16 + 1, 1, 1, 5);
+	RgbLedDriver_TurnOn((4  - i + 16) % 16 + 1, 5, 5, 5);
+	RgbLedDriver_TurnOn((5  - i + 16) % 16 + 1, 5, 1, 1);
+	RgbLedDriver_TurnOn((6  - i + 16) % 16 + 1, 1, 5, 1);
+	RgbLedDriver_TurnOn((7  - i + 16) % 16 + 1, 1, 1, 5);
+	RgbLedDriver_TurnOn((8  - i + 16) % 16 + 1, 5, 5, 5);
+	RgbLedDriver_TurnOn((9  - i + 16) % 16 + 1, 5, 1, 1);
+	RgbLedDriver_TurnOn((10 - i + 16) % 16 + 1, 1, 5, 1);
+	RgbLedDriver_TurnOn((11 - i + 16) % 16 + 1, 1, 1, 5);
+	RgbLedDriver_TurnOn((12 - i + 16) % 16 + 1, 5, 5, 5);
+	RgbLedDriver_TurnOn((13 - i + 16) % 16 + 1, 5, 1, 1);
+	RgbLedDriver_TurnOn((14 - i + 16) % 16 + 1, 1, 5, 1);
+	RgbLedDriver_TurnOn((15 - i + 16) % 16 + 1, 1, 1, 5);
+	RgbLedDriver_TurnOn((16 - i + 16) % 16 + 1, 5, 5, 5);
+	RotaryEncoder_Read();
+	i = RotaryEncoder_GetRotation();
+}
+
+int main(void)
+{
+	/* Hold the watchdog timer so it doesn't reset our chip */
+	WDTCTL = WDTPW + WDTHOLD;
+	initializeEncoderRegisters();
+	initializeLedRegisters();
+	initializeClock();
 
 	RgbLedDriver_Create();
 	RotaryEncoder_Create((uint8_t *)(&P2IN), 6, 7);
 
-	int i = 0;
 	/* infinite loop */
 	for( ; ; ) {
-		RgbLedDriver_TurnOn((1  - i + 16) % 16 + 1, 5, 1, 1);
-		RgbLedDriver_TurnOn((2  - i + 16) % 16 + 1, 1, 5, 1);
-		RgbLedDriver_TurnOn((3  - i + 16) % 16 + 1, 1, 1, 5);
-		RgbLedDriver_TurnOn((4  - i + 16) % 16 + 1, 5, 5, 5);
-		RgbLedDriver_TurnOn((5  - i + 16) % 16 + 1, 5, 1, 1);
-		RgbLedDriver_TurnOn((6  - i + 16) % 16 + 1, 1, 5, 1);
-		RgbLedDriver_TurnOn((7  - i + 16) % 16 + 1, 1, 1, 5);
-		RgbLedDriver_TurnOn((8  - i + 16) % 16 + 1, 5, 5, 5);
-		RgbLedDriver_TurnOn((9  - i + 16) % 16 + 1, 5, 1, 1);
-		RgbLedDriver_TurnOn((10 - i + 16) % 16 + 1, 1, 5, 1);
-		RgbLedDriver_TurnOn((11 - i + 16) % 16 + 1, 1, 1, 5);
-		RgbLedDriver_TurnOn((12 - i + 16) % 16 + 1, 5, 5, 5);
-		RgbLedDriver_TurnOn((13 - i + 16) % 16 + 1, 5, 1, 1);
-		RgbLedDriver_TurnOn((14 - i + 16) % 16 + 1, 1, 5, 1);
-		RgbLedDriver_TurnOn((15 - i + 16) % 16 + 1, 1, 1, 5);
-		RgbLedDriver_TurnOn((16 - i + 16) % 16 + 1, 5, 5, 5);
-		RotaryEncoder_Read();
-		i = RotaryEncoder_GetRotation();
+		loop();
 	}
 }
